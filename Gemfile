167da1835dd8be074c07de60acc9e4a67d64b6b1
Tom Scott <tscott@telvue.com>
2014-03-22 02:19:05 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '~> 4.0'
+gem 'pg'
+gem 'active_model_serializers'
+
+gem 'sass-rails'
+gem 'coffee-rails'
+gem 'zurb-foundation',  '>= 4.0'
+gem 'uglifier',         '~> 1.3'
+
+gem 'jquery-rails',     '2.2.2'
+gem 'ember-rails'
+gem 'emblem-rails'
+gem 'ember_script-rails'
+
+group :development do
+  gem 'meta_request'
+end
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'teaspoon'
+end
+
+group :test do
+  gem 'rspec-rails'
+end
+
+group :production do
+  gem 'rack-cache'
+  gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eye::Application.config.secret_key_base = '423b56c07a0a403c7ee9fd5ac4dc7d76d0a418ce59f634a14fc1425a8403622476e52455bbe59b50716eb51cadcda9131fa956ed5bb04eefe83c2c278d34c349'
